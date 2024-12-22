09c865b2dab6c3e15b0c5b6e03727fec175fe641
Phan Tuan Giang <tuangiangdev@gmail.com>
2024-12-22 14:14:37 +0700

    Env

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require("express");
-
 const routes = require("./routes/client/index.route");
+require("dotenv").config();
+
 const app = express();
-const port = 8080;
+const port = process.env.PORT;
 
 app.set("views", "./views");
 app.set("view engine", "pug");
